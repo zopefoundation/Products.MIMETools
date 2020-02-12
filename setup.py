6a02e41dfef6544593f47062b5962d120e5cdaff
Jens Vagelpohl <jens@netz.ooo>
2020-02-11 19:27:42 -0600

    - vb [ci skip]

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 
 setup(name='Products.MIMETools',
-      version='3.1',
+      version='3.2.dev0',
       url='https://github.com/zopefoundation/Products.MIMETools',
       project_urls={
         'Issue Tracker': ('https://github.com/zopefoundation'
